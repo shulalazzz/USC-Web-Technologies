58874277288f7c86c3d53f84de366c3418d2d0aa
Jing <zj2060203064@gmail.com>
2023-03-21 13:39:49 -0700

    Update app.module.ts

--- HW8/frontend/src/app/app.module.ts
+++ HW8/frontend/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { SearchRouterComponent } from './components/search-router/search-router.component';
-import { FavoritesRouterComponent } from './components/favorites-router/favorites-router.component';
-import { SearchFormComponent } from './components/search-form/search-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { SearchRouterComponent } from './components/search-router/search-router.component';
+import { FavoritesRouterComponent } from './components/favorites-router/favorites-router.component';
 import { ResultsTableComponent } from './components/results-table/results-table.component';
 import { NoResultsComponent } from './components/no-results/no-results.component';
 
